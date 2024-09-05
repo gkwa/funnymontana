1c1a5fd2d0a98b6c93187511a2fea60207447192
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-04 22:12:57 -0400

    improve prompt

--- utils.ts
+++ utils.ts
@@ -103,7 +103,7 @@ Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
 
-
+Notice how there are two pargraphs there.
 
 
 1. Put the title of the page on a separate line at the top 
